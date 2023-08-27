7df589a31a1965f37bbe6ffd9d38c4ddf55ac6db
Pascal <pascal.reinhardt@gmx.net>
2023-08-27 06:01:29 +0200

    Redundanz ein klein wenig vermindert

--- src/Main.java
+++ src/Main.java
@@ -27,11 +27,9 @@ public class Main {
             System.out.println("Bitte geben Sie die Operanden ein (Gleitkomma, getrennt durch ein Leerzeichen)!: ");
 
                 String operandenUndefined = eingabe.nextLine();
-
                 String[] operandenString = operandenUndefined.split(" ");
 
                 operanden.clear();
-
 
                 for (String operand: operandenString) {
                     if (!operand.isEmpty())
@@ -41,43 +39,29 @@ public class Main {
                 System.out.println("Nun bitte die Rechenoperation angeben: \n a-addieren, s-subtrahieren, m-multiplizieren, d-dividieren.E für escape.");
                 String operation = eingabe.nextLine();
 
-                switch (operation) {
-                    case ("a"):
-                        ergebnis = operanden.get(0);
-                        for (int u = 1; u < operanden.size(); u++) {
-                            ergebnis += operanden.get(u);
-                        }
-                        System.out.println(ergebnis);
-                        break;
-                    case "s":
-                        ergebnis = operanden.get(0);
-                        for (int u = 1; u < operanden.size(); u++) {
-                            ergebnis -= operanden.get(u);
-                        }
-                        System.out.println(ergebnis);
-                        break;
-                    case "m":
-                        ergebnis = operanden.get(0);
-                        for (int u = 1; u < operanden.size(); u++) {
-                            ergebnis *= operanden.get(u);
-                        }
-                        System.out.println(ergebnis);
-                        break;
-                    case "d":
-                        ergebnis = operanden.get(0);
-                        for (int u = 1; u < operanden.size(); u++) {
-                            ergebnis /= operanden.get(u);
-                        }
-                        System.out.println(ergebnis);
-                        break;
-                    case "E":
-                        bool = false;
-                        break;
-                    default:
-                        System.out.println("Keine gültige Eingabe.");
-                        break;
-
+            for (int u = 1; u < operanden.size(); u++) {
+                ergebnis = operanden.get(0);
+                if(operation.equals("a")){
+                    ergebnis += operanden.get(u);
+                    System.out.println(ergebnis);
                 }
+                if(operation.equals("s")){
+                    ergebnis -= operanden.get(u);
+                    System.out.println(ergebnis);
+                }
+                if(operation.equals("m")){
+                    ergebnis *= operanden.get(u);
+                    System.out.println(ergebnis);
+                }
+                if(operation.equals("d")){
+                    ergebnis /= operanden.get(u);
+                    System.out.println(ergebnis);
+                }
+                if(operation.equals("E")){
+                    bool = false;
+                }
+            }
+
 
             }
 
